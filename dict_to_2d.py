b66f1564055d3bab0239daf9474a0e6629599ba7
edvac <edvac@mail.ru>
2022-02-03 11:22:57 +0300

    refactoring code rename function  dict_to_2d -> dict_to_2d_dict delete function  add_to result.

--- dict_to_2d.py
+++ dict_to_2d.py
@@ -1,14 +1,12 @@
-
+"""
+    functions for prepare dicts
+"""
 
 def join_path(root: str, path: str, delimiter: str = "."):
     return delimiter.join(filter(None, (root, path)))
 
 
-def add_to_result(key_list: list, value: (str, int, bool, None)):
-    return ".".join(key_list), value
-
-
-def dict_to_2d(arr: (dict, list), root: str = None):
+def dict_to_2d_dict(arr: (dict, list), root: str = None):
     if not isinstance(arr, (dict, list)):
         raise ValueError("Not dict or list")
     result = {}
@@ -23,29 +21,8 @@ def dict_to_2d(arr: (dict, list), root: str = None):
             result[join_path(root, key)] = value
         elif isinstance(value, (dict, list)):
             result.update({
-                join_path(root, key): value for key, value in dict_to_2d(value, key).items()
+                join_path(root, key): value for key, value in dict_to_2d_dict(value, key).items()
             })
-    return result
-
-
-def dict_to_2d_mode(arr: (dict, list), root: str = None):
-    result = {}
-    if not isinstance(arr, (dict, list)):
-        raise ValueError("Not dict or list")
-    if isinstance(arr, (dict, list)):
-        path_prefix = "#" if isinstance(arr, list) else None
-        for item_key in (
-                arr if isinstance(arr, dict)
-                else range(len(arr))
-        ):
-            key = join_path(path_prefix, str(item_key), "")
-            value = arr[item_key]
-            if isinstance(value, (int, str, bool, float)):
-                result[join_path(root, key)] = value
-            elif isinstance(value, (dict, list)):
-                result.update({
-                    join_path(root, key): value for key, value in dict_to_2d_mode(value, key).items()
-                })
     return result
 
 
